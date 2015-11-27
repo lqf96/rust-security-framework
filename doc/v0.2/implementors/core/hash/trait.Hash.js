325ec842ddc27cc4af65411a5221c05887066644
Steven Fackler <sfackler@gmail.com>
2015-11-27 17:15:42 -0500

    Docs

---
+++ doc/v0.2/implementors/core/hash/trait.Hash.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["security_framework"] = ["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/hash/trait.Hash.html\" title=\"trait core::hash::Hash\">Hash</a> for <a class=\"struct\" href=\"security_framework/cipher_suite/struct.CipherSuite.html\" title=\"struct security_framework::cipher_suite::CipherSuite\">CipherSuite</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ doc/v0.2/implementors/core_foundation/base/trait.TCFType.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["security_framework"] = ["impl TCFType for <a class=\"struct\" href=\"security_framework/certificate/struct.SecCertificate.html\" title=\"struct security_framework::certificate::SecCertificate\">SecCertificate</a>","impl TCFType for <a class=\"struct\" href=\"security_framework/identity/struct.SecIdentity.html\" title=\"struct security_framework::identity::SecIdentity\">SecIdentity</a>","impl TCFType for <a class=\"struct\" href=\"security_framework/key/struct.SecKey.html\" title=\"struct security_framework::key::SecKey\">SecKey</a>","impl TCFType for <a class=\"struct\" href=\"security_framework/os/macos/access/struct.SecAccess.html\" title=\"struct security_framework::os::macos::access::SecAccess\">SecAccess</a>","impl TCFType for <a class=\"struct\" href=\"security_framework/os/macos/keychain/struct.SecKeychain.html\" title=\"struct security_framework::os::macos::keychain::SecKeychain\">SecKeychain</a>","impl TCFType for <a class=\"struct\" href=\"security_framework/os/macos/keychain_item/struct.SecKeychainItem.html\" title=\"struct security_framework::os::macos::keychain_item::SecKeychainItem\">SecKeychainItem</a>","impl TCFType for <a class=\"struct\" href=\"security_framework/os/macos/transform/struct.SecTransform.html\" title=\"struct security_framework::os::macos::transform::SecTransform\">SecTransform</a>","impl TCFType for <a class=\"struct\" href=\"security_framework/policy/struct.SecPolicy.html\" title=\"struct security_framework::policy::SecPolicy\">SecPolicy</a>","impl TCFType for <a class=\"struct\" href=\"security_framework/secure_transport/struct.SslContext.html\" title=\"struct security_framework::secure_transport::SslContext\">SslContext</a>","impl TCFType for <a class=\"struct\" href=\"security_framework/trust/struct.SecTrust.html\" title=\"struct security_framework::trust::SecTrust\">SecTrust</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
